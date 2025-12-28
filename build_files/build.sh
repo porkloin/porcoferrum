ac35b7c1e7c82ab17b3a9f50be2307e70dcee80d
Ryan Bateman <rybateman@gmail.com>
2025-12-28 10:46:40 -0900

    porcoferrum i summon u to a cursed existence

--- build_files/build.sh
+++ build_files/build.sh
@@ -10,7 +10,11 @@ set -ouex pipefail
 # https://mirrors.rpmfusion.org/mirrorlist?path=free/fedora/updates/39/x86_64/repoview/index.html&protocol=https&redirect=1
 
 # this installs a package from fedora repos
-dnf5 install -y tmux 
+dnf5 install -y tmux
+
+# coolercontrol
+dnf5 -y copr enable codifryed/CoolerControl
+dnf5 install -y coolercontrol
 
 # Use a COPR Example:
 #
@@ -22,3 +26,6 @@ dnf5 install -y tmux
 #### Example for enabling a System Unit File
 
 systemctl enable podman.socket
+
+# Enable systemd unit for coolercontrol
+systemctl enable coolercontrold.service
