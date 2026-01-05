2934165bf159b613ab18fee534a65f37a7d5111e
Ryan Bateman <rybateman@gmail.com>
2026-01-04 15:27:21 -0900

    add gamescope session

--- build_files/build.sh
+++ build_files/build.sh
@@ -8,8 +8,8 @@ dnf5 -y copr enable lizardbyte/beta
 dnf5 -y config-manager addrepo --from-repofile=https://negativo17.org/repos/fedora-steam.repo
 
 # fresh DMS regardless of zirc upstream updates
-dnf -y copr enable avengemedia/dms-git
-dnf -y \
+dnf5 -y copr enable avengemedia/dms-git
+dnf5 -y \
   --enablerepo copr:copr.fedorainfracloud.org:avengemedia:dms-git \
   --enablerepo copr:copr.fedorainfracloud.org:avengemedia:danklinux \
   distro-sync \
@@ -19,7 +19,7 @@ dnf -y \
   dms-greeter \
   dgop \
   dsearch
-dnf -y copr disable avengemedia/dms-git
+dnf5 -y copr disable avengemedia/dms-git
 
 # install extra shit
 dnf5 install -y \
@@ -34,6 +34,14 @@ dnf5 install -y \
 # disable COPRs
 dnf5 -y copr disable codifryed/CoolerControl
 dnf5 -y copr disable lizardbyte/beta
+
+# gamescope session
+mkdir -p /usr/share/gamescope-session-plus/
+curl --retry 3 -Lo /usr/share/gamescope-session-plus/bootstrap_steam.tar.gz https://large-package-sources.nobaraproject.org/bootstrap_steam.tar.gz
+dnf5 -y install \
+  --repo copr:copr.fedorainfracloud.org:ublue-os:bazzite \
+  gamescope-session-plus \
+  gamescope-session-steam
 
 # enable units
 systemctl enable coolercontrold.service
