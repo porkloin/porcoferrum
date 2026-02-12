38506294f26e29558a863b1e704a530087d3130a
Ryan Bateman <rybateman@gmail.com>
2026-02-11 23:11:30 -0900

    just get scb from terra

--- build_files/build.sh
+++ build_files/build.sh
@@ -3,51 +3,48 @@ set -xeuo pipefail
 
 rm -f /etc/profile.d/gamerslop.sh
 
+dnf5 install -y ScopeBuddy
+
 dnf5 -y copr enable codifryed/CoolerControl
 dnf5 install -y coolercontrol liquidctl
 dnf5 -y copr disable codifryed/CoolerControl
 
-dnf -y copr enable bieszczaders/kernel-cachyos
-dnf -y copr disable bieszczaders/kernel-cachyos
-
-dnf5 -y --enablerepo=copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos \
-  install \
-  kernel-cachyos \
-  kernel-cachyos-devel-matched
-
-KERNEL_VERSION="$(ls /usr/lib/modules | sort -V | tail -n1)"
-KERNEL_SRC="/usr/src/kernels/${KERNEL_VERSION}"
-
-echo "== Target kernel: ${KERNEL_VERSION} =="
-
-if [[ ! -d "${KERNEL_SRC}" ]]; then
-  echo "ERROR: Missing kernel headers at ${KERNEL_SRC}"
-  exit 1
-fi
-
-git clone --depth=1 https://github.com/dlundqvist/xone /tmp/xone
-pushd /tmp/xone
-
-make -C "${KERNEL_SRC}" M="$PWD" modules
-make -C "${KERNEL_SRC}" M="$PWD" modules_install
-
-dnf5 install -y bsdtar
-cd /tmp/xone
-./install/firmware.sh --skip-disclaimer
-
-popd
-
-depmod -a "${KERNEL_VERSION}"
-
-if ! find "/usr/lib/modules/${KERNEL_VERSION}" -iname 'xone*.ko*' | grep -q .; then
-  echo "ERROR: xone module not installed"
-  exit 1
-fi
-
-curl -Lo /usr/local/bin/scopebuddy \
-  https://raw.githubusercontent.com/OpenGamingCollective/ScopeBuddy/refs/heads/main/bin/scopebuddy
-chmod +x /usr/local/bin/scopebuddy
-ln -sf scopebuddy /usr/local/bin/scb
+# dnf -y copr enable bieszczaders/kernel-cachyos
+# dnf -y copr disable bieszczaders/kernel-cachyos
+#
+# dnf5 -y --enablerepo=copr:copr.fedorainfracloud.org:bieszczaders:kernel-cachyos \
+#   install \
+#   kernel-cachyos \
+#   kernel-cachyos-devel-matched
+#
+# KERNEL_VERSION="$(ls /usr/lib/modules | sort -V | tail -n1)"
+# KERNEL_SRC="/usr/src/kernels/${KERNEL_VERSION}"
+#
+# echo "== Target kernel: ${KERNEL_VERSION} =="
+#
+# if [[ ! -d "${KERNEL_SRC}" ]]; then
+#   echo "ERROR: Missing kernel headers at ${KERNEL_SRC}"
+#   exit 1
+# fi
+#
+# git clone --depth=1 https://github.com/dlundqvist/xone /tmp/xone
+# pushd /tmp/xone
+#
+# make -C "${KERNEL_SRC}" M="$PWD" modules
+# make -C "${KERNEL_SRC}" M="$PWD" modules_install
+#
+# dnf5 install -y bsdtar
+# cd /tmp/xone
+# ./install/firmware.sh --skip-disclaimer
+#
+# popd
+#
+# depmod -a "${KERNEL_VERSION}"
+#
+# if ! find "/usr/lib/modules/${KERNEL_VERSION}" -iname 'xone*.ko*' | grep -q .; then
+#   echo "ERROR: xone module not installed"
+#   exit 1
+# fi
 
 # Moonshine dependencies:
 
